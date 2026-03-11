1aa45efbec88fd8f573ad7ea27fb0f4d3759188b
luigi beraldi <luiggiberaldi94@gmail.com>
2026-03-11 00:41:00 -0400

    style: remove grid container from Metar and Alerts modules to display on separate rows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,17 +19,14 @@ export default function DashboardPage() {
         <CurrentForecast />
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        
-        {/* Módulo 2: METAR / TAF / GAMET */}
-        <div id="metar" className="scroll-mt-6">
-          <MetarTafGamet />
-        </div>
+      {/* Módulo 2: METAR / TAF / GAMET */}
+      <div id="metar" className="scroll-mt-6">
+        <MetarTafGamet />
+      </div>
 
-        {/* Módulo 3: Alertas Operacionales */}
-        <div id="alertas" className="scroll-mt-6">
-          <OperationalAlerts />
-        </div>
+      {/* Módulo 3: Alertas Operacionales */}
+      <div id="alertas" className="scroll-mt-6">
+        <OperationalAlerts />
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
